e8dd482f95f82061ac04a0a8b49760e5e6724784
LRS4 <lrspencer@hotmail.co.uk>
2020-09-30 00:30:37 +0100

    Add outline to model.py

--- model.py
+++ model.py
@@ -1,29 +1,33 @@
-from tpot import TPOTClassifier
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-import numpy as np
+import os
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
+from sklearn.impute import SimpleImputer
+from transformers import ColumnsSelector
+from pipeline import exported_pipeline as classifier
 
-iris = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
-iris = iris.rename(columns={'variety': 'class'})
-iris['class'] = iris['class'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
+from config import (X_TRAIN, X_TEST, Y_TRAIN, Y_TEST, DATA_DIR,
+                    DATA_FILENAME, TARGET_COLUMN_NAME, PARAMS)
 
-print(f"All data: {iris.shape}")
 
-X_train, X_test, y_train, y_test = train_test_split(iris.drop(['class'], axis=1).astype(np.float64),
-    iris['class'].astype(np.float64), train_size=0.8, test_size=0.2, random_state=42)
+def load_dataset(x_path, y_path):
+    """
+    Loads  X (features) and y (targets) for train or test sets
+    """
+    X = pd.read_csv(os.sep.join([DATA_DIR, x_path]))
+    y = pd.read_csv(os.sep.join([DATA_DIR, y_path]))
+    return X, y
 
-print(f'X_train: {X_train.shape}')
-print(f'y_train: {y_train.shape}')
-print(f'X_test: {X_test.shape}')
-print(f'y_test: {y_test.shape}')
 
-tpot = TPOTClassifier(generations=5,
-                      population_size=50,
-                      verbosity=2, 
-                      random_state=42, 
-                      max_time_mins=None)
+def build_model():
+    pass
 
-tpot.fit(X_train, y_train)
-print(tpot.score(X_test, y_test))
-tpot.export('tpot_iris_pipeline.py') 
+
+def train_model():
+    pass
+
+
+def tune_pipeline():
+    pass
